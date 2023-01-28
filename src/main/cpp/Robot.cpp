0b27c68c7eef7e01d36bfc259510be28d610ed23
keanecm <98241890+Melbourne-Robocats-Student@users.noreply.github.com>
2023-01-28 16:52:30 +1100

    Added LEDs :smile:

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -22,6 +22,18 @@
 #include <frc/Encoder.h>
 #include <frc/AnalogGyro.h>
 #include <frc/Timer.h>
+#include <frc/AddressableLED.h>
+
+//LED includes
+
+  static constexpr int kLength = 5;
+
+  // Must be a PWM header, not MXP or DIO
+  frc::AddressableLED m_led{4};
+  std::array<frc::AddressableLED::LEDData, kLength>
+      m_ledBuffer;  // Reuse the buffer
+  // Store what the last hue of the first pixel is
+  int firstPixelHue = 0;
 
 #define BOOST_SPEED (0.7)
 #define NORMAL_SPEED (0.55)
@@ -76,6 +88,18 @@ cs::UsbCamera m_camera_drive;
 nt::NetworkTableEntry m_cameraSelection;
 
 void Robot::RobotInit() {
+
+  // Default to a length of 60, start empty output
+  // Length is expensive to set, so only set it once, then just update data
+  m_led.SetLength(kLength);
+  m_led.SetData(m_ledBuffer);
+  m_led.Start();
+  for (int i = 0; i < kLength; i++) {
+      m_ledBuffer[i].SetHSV(250, 255, 59);
+    }
+  // Set the LEDs
+  m_led.SetData(m_ledBuffer);
+
   // Debug encoder to dashboard
   m_chooser.SetDefaultOption(kAutoNameDefault, kAutoNameDefault);
   m_chooser.AddOption(kAutoNameCustom, kAutoNameCustom);
@@ -111,8 +135,7 @@ void Robot::RobotInit() {
  * <p> This runs after the mode specific periodic functions, but before
  * LiveWindow and SmartDashboard integrated updating.
  */
-void Robot::RobotPeriodic() {
-}
+void Robot::RobotPeriodic() {}
 
 /**
  * This autonomous (along with the chooser code above) shows how to select
@@ -209,7 +232,8 @@ void Robot::AutonomousPeriodic() {
   }
 }
 
-void Robot::TeleopInit() {}
+void Robot::TeleopInit() {
+}
 
 void Robot::TeleopPeriodic() {
   double m_maxspeed;
@@ -238,7 +262,7 @@ void Robot::TeleopPeriodic() {
 
   }
 
-  if (m_xbox.GetPOV(180)==-1)
+  if (m_xbox.GetPOV()==180)
   {
     m_vertical.Set(-0.6);
 
@@ -284,6 +308,33 @@ void Robot::TeleopPeriodic() {
   }
   if (m_xbox.GetBButton()) {
     m_cameraSelection.SetString(m_camera_drive.GetName());
+  }
+
+  if(m_joystick.GetRawButtonPressed(11) == 1){
+    // For every pixel
+    for (int i = 0; i < kLength; i++) {
+    //   // Calculate the hue - hue is easier for rainbows because the color
+    //   // shape is a circle so only one value needs to precess
+      const auto pixelHue = (firstPixelHue + (i * 180 / kLength)) % 180;
+    //   // Set the value
+      m_ledBuffer[i].SetHSV(pixelHue, 255, 128);
+    }
+    // // Increase by to make the rainbow "move"
+    firstPixelHue += 3;
+    // // Check bounds
+    firstPixelHue %= 180;
+
+     // Set the LEDs
+    m_led.SetData(m_ledBuffer);
+  }
+
+  //purple
+  if(m_joystick.GetRawButtonPressed(12)){
+    for (int i = 0; i < kLength; i++) {
+      m_ledBuffer[i].SetHSV(20, 255, 128);
+    }
+     // Set the LEDs
+    m_led.SetData(m_ledBuffer);
   }
 
 }
